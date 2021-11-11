d7237629201ccb3d28f252612b21406c02184aeb
andreiursan23 <ursanandrei@ymail.com>
2021-11-11 19:46:58 +0200

    Added width and height on img

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -10,12 +10,20 @@ function SingleCard({ card, handleChoice, flipped, disabled }) {
   return (
     <div className="card">
       <div className={flipped ? "flipped" : ""}>
-        <img src={card.src} alt="card-front" className="front" />
+        <img
+          src={card.src}
+          alt="card-front"
+          className="front"
+          height="300px"
+          width="300px"
+        />
         <img
           src="/img/ferrari-logo-back.png"
           alt="card-back"
           onClick={handleClick}
           className="back"
+          height="300px"
+          width="300px"
         />
       </div>
     </div>
